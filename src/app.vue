20fcb31a588a068dbc1d4b8ec89c65552be3ff40
majun1@jd.com <majun1@jd.com>
2017-02-10 15:29:15 +0800

    init

---
+++ src/app.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="content">
+    <h1>pangolin</h1>
+    <p>A vue hi.</p>
+    <el-button>默认按钮</el-button>
+  </div>
+</template>
+<script>
+  import Vue from 'vue';
+  import {
+    Button
+  } from 'element-ui';
+
+  Vue.use(Button);
+
+  export default {
+    name: 'app'
+  };
+</script>
+<style scoped>
+  .content {
+    font-size: 12px;
+  }
+</style>
